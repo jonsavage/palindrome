2d43629b7cdad588880116b1e72dec1668a72892
Jon Savage <jon5@vt.edu>
2016-12-09 15:42:11 -0500

    Added Test for Single Character Palindromes

--- palindrome_test.go
+++ palindrome_test.go
@@ -10,3 +10,9 @@ func Test_IsPalindrome_WithEmptyString_ReturnsFalse(t *testing.T) {
 
     assert.False(t, result, "Empty strings are not palindromes")
 }
+
+func Test_IsPalindrome_WithSingleCharacterString_ReturnsTrue(t *testing.T) {
+    result := IsPalindrome("a")
+
+    assert.True(t, result, "Single character strings are palindromes")
+}
