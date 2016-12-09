286ec93a8e0f6fc32bf1bca00efcfbd5b8a49fc2
Jon Savage <jon5@vt.edu>
2016-12-09 17:34:18 -0500

    Refactor Tests to DRY out Test Internals
    DRYed the call to IsPalindrome and parameterized the call to assert.False.

--- palindrome_test.go
+++ palindrome_test.go
@@ -6,37 +6,37 @@ import (
 )
 
 func Test_IsPalindrome_WithEmptyString_ReturnsFalse(t *testing.T) {
-    result := IsPalindrome("")
-
-    assert.False(t, result, "Empty strings are not palindromes")
+    Assert_IsNotPalindrome(t, "")
 }
 
 func Test_IsPalindrome_WithSingleCharacterString_ReturnsTrue(t *testing.T) {
-    result := IsPalindrome("a")
-
-    assert.True(t, result, "Single character strings are palindromes")
+    Assert_IsPalindrome(t, "a")
 }
 
 func Test_IsPalindrome_WithTwoCharacterValidPalindrome_ReturnsTrue(t *testing.T) {
-    result := IsPalindrome("aa")
-
-    assert.True(t, result, "'aa' is a palindrome")
+    Assert_IsPalindrome(t, "a")
 }
 
 func Test_IsPalindrome_WithTwoCharacterInvalidPalindrome_ReturnsFalse(t *testing.T) {
-    result := IsPalindrome("ab")
-
-    assert.False(t, result, "'ab' is not a palindrome")
+    Assert_IsNotPalindrome(t, "ab")
 }
 
 func Test_IsPalindrome_WithThreeCharacterInvalidPalindrome_ReturnsFalse(t *testing.T) {
-    result := IsPalindrome("abc")
-
-    assert.False(t, result, "'abc' is not a palindrome") 
+    Assert_IsNotPalindrome(t, "abc")
 }
 
 func Test_IsPalindrome_WithThreeCharacterValidPalindrome_ReturnsTrue(t *testing.T) {
-    result := IsPalindrome("aba")
+    Assert_IsPalindrome(t, "aba")
+}
 
-    assert.True(t, result, "'aba' is a palindrome") 
+func Assert_IsPalindrome(t *testing.T, candidate string) {
+    result := IsPalindrome(candidate)
+
+    assert.True(t, result, candidate + " is a palindrome")
+}
+
+func Assert_IsNotPalindrome(t *testing.T, candidate string) {
+    result := IsPalindrome(candidate)
+
+    assert.False(t, result, candidate + " is NOT a palindrome")
 }
