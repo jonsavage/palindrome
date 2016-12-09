2dca184d4266344a1d1fa97f2adf09ae5f20bb41
Jon Savage <jon5@vt.edu>
2016-12-09 17:33:35 -0500

    Refactor to Recursive Implementation
    Refactored to lean on recursion

--- palindrome.go
+++ palindrome.go
@@ -7,6 +7,23 @@ func IsPalindrome(candidate string) bool {
     if len(candidate) == 1 {
         return true
     }
+    return isMirrored(candidate)
+}
 
+func isMirrored(candidate string) bool {
+    if len(candidate) <= 1 {
+        return true
+    }
+    if firstAndLastCharactersMatch(candidate) {
+        return isMirrored(trimFirstAndLastCharacters(candidate))
+    }
+    return false
+}
+
+func firstAndLastCharactersMatch(candidate string) bool {
     return candidate[0] == candidate[len(candidate)-1]
+}
+
+func trimFirstAndLastCharacters(aString string) string {
+    return aString[1:len(aString) - 1]
 }
